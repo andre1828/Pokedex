13e70de1a4384f1e78e27ec0d7ab2069b53b2123
andre1828 <andremelo13@gmail.com>
2023-03-01 18:01:51 -0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    sourcemap: true
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      reporter: ['html', 'lcov']
+    }
+  }
+});
