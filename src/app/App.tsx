9ea3e42e59111fe7b85a855a21fa5e2d77f08724
andre1828 <andremelo13@gmail.com>
2023-03-01 18:23:03 -0300

    build: show pokemon from pokemons.json
    used as test for deploying on netlify

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,17 @@
 import './App.css';
+import pokemons from 'src/assets/pokemons.json';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ul>
+        <article>
+          <p>{pokemons.results[0].name}</p>
+          <img src={pokemons.results[0].sprites.large} alt="" />
+        </article>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
