d62fe2c3bff0a0a975a99d314540af3c56c4e3eb
andre1828 <andremelo13@gmail.com>
2023-03-03 20:31:03 -0300

    feat: add Search component

--- src/components/pokedex/Pokedex.tsx
+++ src/components/pokedex/Pokedex.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import pokemons from 'src/assets/pokemons.json';
 import Pokemon from 'src/components/pokemon';
 import bg from 'src/assets/body_bg.png';
+import Search from 'src/components/search';
+import { useState } from 'react';
 
 const Container = styled.div`
   width: 90%;
@@ -20,10 +22,32 @@ const PokemonList = styled.ul`
 `;
 
 const Pokedex = () => {
+  const [searchResults, setSearchResults] = useState(pokemons.results);
+
+  const handleSearchChange = (searchTerm: string) => {
+    if (searchTerm === '') {
+      setSearchResults(pokemons.results);
+      return;
+    }
+
+    const term = searchTerm.toLowerCase();
+    const regex = new RegExp(term, 'gi');
+    const isSeachByname = Number.isNaN(Number.parseInt(term));
+    let searchResults;
+    if (isSeachByname) {
+      searchResults = pokemons.results.filter((pokemon) => pokemon.name.match(regex));
+    } else {
+      searchResults = pokemons.results.filter((pokemon) => pokemon.national_number.match(regex));
+    }
+
+    setSearchResults(searchResults);
+  };
+
   return (
     <Container>
+      <Search handleSearchChange={handleSearchChange} />
       <PokemonList>
-        {pokemons.results.map((pokemon) => (
+        {searchResults.map((pokemon) => (
           <li key={uuid()}>
             <Pokemon nationalNumber={pokemon.national_number} name={pokemon.name} type={pokemon.type} />
           </li>
---
+++ src/components/search/Search.tsx
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+import searchIcon from 'src/assets/input-search-bg.png';
+import { useState } from 'react';
+
+const Container = styled.div`
+  display: flex;
+  height: 196px;
+  padding-left: 158px;
+  background-color: #424242;
+`;
+const SearchTitle = styled.p`
+  color: #fff;
+  margin-top: 0px;
+  font-size: 167.5%;
+`;
+const SearchInput = styled.input`
+  width: 311px;
+  height: 41px;
+  border: 3px solid #616161;
+  border-radius: 5px;
+`;
+
+const SearchButton = styled.button`
+  width: 55px;
+  height: 50px;
+  margin-left: 23px;
+  background-color: #ee6b2f;
+  border: none;
+  border-radius: 5px;
+`;
+
+const Subtitle = styled.p`
+  color: #fff;
+`;
+
+const InfoBox = styled.div`
+  background-color: #4dad5b;
+  height: 87px;
+  align-self: center;
+  width: 429px;
+  margin-left: 53px;
+  border-radius: 5px;
+`;
+
+const InfoText = styled.h4`
+  color: #fff;
+  margin: 0.75em 0.5em 0 1em;
+  font-size: 125%;
+`;
+
+interface SearchProps {
+  handleSearchChange: (searchTerm: string) => void;
+}
+const Search = ({ handleSearchChange }: SearchProps) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <Container>
+      <div>
+        <SearchTitle>Name or Number</SearchTitle>
+        <SearchInput type="text" onKeyUp={(e) => setSearchTerm(e.target.value)} />
+        <SearchButton type="submit" onClick={() => handleSearchChange(searchTerm)}>
+          <img src={searchIcon} alt="" />
+        </SearchButton>
+        <Subtitle>
+          Use the Advanced Search to explore Pokémon by type, <br /> weakness, Ability, and more!
+        </Subtitle>
+      </div>
+      <InfoBox>
+        <InfoText>Search for a pokémon by name or using its National Pokédex number.</InfoText>
+      </InfoBox>
+    </Container>
+  );
+};
+
+export default Search;
