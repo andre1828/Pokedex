533ac5d3bf18ec758c528263fd31437aa7856241
andre1828 <andremelo13@gmail.com>
2023-03-03 20:32:58 -0300

    feat: add maven pro regular font

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import App from '@app/App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '/src/fonts/MavenProRegular/Maven-Pro-Regular.ttf.woff';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
